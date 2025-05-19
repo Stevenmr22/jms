b5cebccb65ccf4889a49ea61b0f18acdb11e0deb
Steven <smmr0001@ce.pucmm.edu.do>
2025-05-19 09:08:43 -0400

    feat: update categories data and add keywords; remove teamMembers data

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,47 +1,63 @@
 export const categories = [
-	{
-		id: 'interior',
-		name: 'Interior',
-		image: '/img/categories/interior.png',
-	},
-	{
-		id: 'banos',
-		name: 'Baños',
-		image: '/img/categories/banos.png',
-	},
-	{
-		id: 'exterior',
-		name: 'Exterior',
-		image: '/img/categories/exterior.png',
-	},
-	{
-		id: 'revestimiento-pared',
-		name: 'Revestimiento de pared',
-		image: '/img/categories/revestimiento-pared.png',
-	},
-	{
-		id: 'griferia',
-		name: 'Grifería',
-		image: '/img/categories/griferia.jpg',
-	},
-	{
-		id: 'ladrillos',
-		name: 'Ladrillos',
-		image: '/img/categories/ladrillos.jpg',
-	},
-	{
-		id: 'adhesivos-derretidos',
-		name: 'Adhesivos y Derretidos',
-		image: '/img/categories/adhesivos-derretidos.png',
-	},
-	{
-		id: 'mamparas-puertas-ventanas',
-		name: 'Mamparas puertas y ventanas',
+    {
+        id: 'ceramica-porcelanato',
+        name: 'Cerámica y Porcelanato',
+        image: '/img/categories/ceramica-porcelanato.png',
+    },
+    {
+        id: 'banos',
+        name: 'Baños',
+        image: '/img/categories/banos.png',
+    },
+    {
+        id: 'cocina',
+        name: 'Cocina',
+        image: '/img/categories/cocina.jpg',
+    },
+    {
+        id: 'griferia',
+        name: 'Grifería',
+        image: '/img/categories/griferia.jpg',
+    },
+    {
+        id: 'mamparas-puertas-ventanas',
+        name: 'Mamparas, Puertas y Ventanas',
 		image: '/img/categories/mamparas-puertas-ventanas.png',
-	},
-	{
-		id: 'fregaderos',
-		name: 'Fregaderos',
-		image: '/img/categories/fregaderos.jpg',
-	},
+    },
+    {
+        id: 'otros',
+        name: 'Otros',
+        image: '/img/categories/otros.png',
+    },
+        {
+        id: 'ceramica-porcelanato',
+        name: 'Cerámica y Porcelanato',
+        image: '/img/categories/ceramica-porcelanato.png',
+    },
+    {
+        id: 'banos',
+        name: 'Baños',
+        image: '/img/categories/banos.png',
+    },
+    {
+        id: 'cocina',
+        name: 'Cocina',
+        image: '/img/categories/cocina.jpg',
+    },
+    {
+        id: 'griferia',
+        name: 'Grifería',
+        image: '/img/categories/griferia.jpg',
+    },
+    {
+        id: 'mamparas-puertas-ventanas',
+        name: 'Mamparas, Puertas y Ventanas',
+		image: '/img/categories/mamparas-puertas-ventanas.png',
+    },
+    {
+        id: 'otros',
+        name: 'Otros',
+        image: '/img/categories/otros.png',
+    },
+    
 ];
