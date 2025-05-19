b11520cfd12a9dfe1c5fc2e64e8162e11f60e6f8
Steven <smmr0001@ce.pucmm.edu.do>
2025-05-19 09:09:56 -0400

    fix: remove duplicate category entries in categories data

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,63 +1,62 @@
 export const categories = [
-    {
-        id: 'ceramica-porcelanato',
-        name: 'Cerámica y Porcelanato',
-        image: '/img/categories/ceramica-porcelanato.png',
-    },
-    {
-        id: 'banos',
-        name: 'Baños',
-        image: '/img/categories/banos.png',
-    },
-    {
-        id: 'cocina',
-        name: 'Cocina',
-        image: '/img/categories/cocina.jpg',
-    },
-    {
-        id: 'griferia',
-        name: 'Grifería',
-        image: '/img/categories/griferia.jpg',
-    },
-    {
-        id: 'mamparas-puertas-ventanas',
-        name: 'Mamparas, Puertas y Ventanas',
+	{
+		id: 'ceramica-porcelanato',
+		name: 'Cerámica y Porcelanato',
+		image: '/img/categories/ceramica-porcelanato.png',
+	},
+	{
+		id: 'banos',
+		name: 'Baños',
+		image: '/img/categories/banos.png',
+	},
+	{
+		id: 'cocina',
+		name: 'Cocina',
+		image: '/img/categories/cocina.jpg',
+	},
+	{
+		id: 'griferia',
+		name: 'Grifería',
+		image: '/img/categories/griferia.jpg',
+	},
+	{
+		id: 'mamparas-puertas-ventanas',
+		name: 'Mamparas, Puertas y Ventanas',
 		image: '/img/categories/mamparas-puertas-ventanas.png',
-    },
-    {
-        id: 'otros',
-        name: 'Otros',
-        image: '/img/categories/otros.png',
-    },
-        {
-        id: 'ceramica-porcelanato',
-        name: 'Cerámica y Porcelanato',
-        image: '/img/categories/ceramica-porcelanato.png',
-    },
-    {
-        id: 'banos',
-        name: 'Baños',
-        image: '/img/categories/banos.png',
-    },
-    {
-        id: 'cocina',
-        name: 'Cocina',
-        image: '/img/categories/cocina.jpg',
-    },
-    {
-        id: 'griferia',
-        name: 'Grifería',
-        image: '/img/categories/griferia.jpg',
-    },
-    {
-        id: 'mamparas-puertas-ventanas',
-        name: 'Mamparas, Puertas y Ventanas',
+	},
+	{
+		id: 'otros',
+		name: 'Otros',
+		image: '/img/categories/otros.png',
+	},
+	{
+		id: 'ceramica-porcelanato',
+		name: 'Cerámica y Porcelanato',
+		image: '/img/categories/ceramica-porcelanato.png',
+	},
+	{
+		id: 'banos',
+		name: 'Baños',
+		image: '/img/categories/banos.png',
+	},
+	{
+		id: 'cocina',
+		name: 'Cocina',
+		image: '/img/categories/cocina.jpg',
+	},
+	{
+		id: 'griferia',
+		name: 'Grifería',
+		image: '/img/categories/griferia.jpg',
+	},
+	{
+		id: 'mamparas-puertas-ventanas',
+		name: 'Mamparas, Puertas y Ventanas',
 		image: '/img/categories/mamparas-puertas-ventanas.png',
-    },
-    {
-        id: 'otros',
-        name: 'Otros',
-        image: '/img/categories/otros.png',
-    },
-    
+	},
+	{
+		id: 'otros',
+		name: 'Otros',
+		image: '/img/categories/otros.png',
+	},
 ];
